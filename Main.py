40a875cea8b8d875cdad9fcdf4392df1f2750eac
turma tecnica <turmatecnicaismael@gmail.com>
2025-08-06 11:16:09 -0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+nome = input ("digite seu nome:")
+idade = int(input("digite uma frase do seu dia:"))
+
+frasedoseudia = input("digite um numero:)
+A = int(inpuit("digite um numero"))
+B = int(input("digite outro  numero:"))
+Soma = A + B
+
+print(nome, idade, frasedoseudia, )
+print(A, B, Soma)
